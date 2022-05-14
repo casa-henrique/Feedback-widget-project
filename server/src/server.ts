60898ce30d28bec16ef9738686ba66c57b85b5ed
casa-henrique <riquecasa@gmail.com>
2022-05-14 16:55:48 -0300

    adding cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,15 @@
+import cors from "cors";
 import express from "express";
 
 import { routes } from "./routes";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "https://hcurriculum.vercel.app/",
+  })
+);
 app.use(express.json());
 app.use(routes);
 
